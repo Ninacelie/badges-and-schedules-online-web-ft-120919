5adcfc82cfd3776d33751d9017bee993ef5448b7
Nina Angelo <ninacelie@gmail.com>
2019-12-11 14:15:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,20 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-   #create new array to put name inside
    array = []
-   #loop through each name provided and push the message to new array with the name
    attendees.each { |name| array << badge_maker(name) }
-   array #return the new array
+   array 
+end
+
+def assign_rooms(attendees)
+   array = []
+   attendees.each_with_index do |name, index|
+      array << "Hello, #{name}! You'll be assigned to room #{index+1}!" 
+   end
+   array
+end
+
+def printer(attendees)
+   batch_badge_creator(attendees).each { |names| puts names }
+   assign_rooms(attendees).each { |room| puts room }
 end
